f91e672247c0cf5661d5347406f7a291c236b675
Luke Sturgill <lukesturgill@gmail.com>
2022-04-23 18:41:28 -0600

    added function for search and api call to js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,39 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
-var discr = document.querySelector('.discr');
-var temp = document.querySelector('.temp');
+const cityNameInput = document.querySelector("#cityName");
+const searchForm = document.querySelector("#searchForm");
+const currentForecastUl = document.querySelector("#currentForecast #weatherConditions");
+const currentForecastH3 = document.querySelector("#currentForecast h3");
+const searchHistory = document.querySelector("#searchHistory");
+const searchHistoryContainer = document.querySelector("#searchHistory .cardBody");
+const dailyForecastContainer = document.querySelector("#dailyForecast");
+const fiveDay = document.querySelector("#fiveDay");
 
-button.addEventListener('click', function(){
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=b06c8666b0489ca67a5e17b0b7bfba15')
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data ['name'];
-        var tempValue = data['main']['temp'];
-        var discrValue = data['weather'][0]['description'];
+// Use local storage to save previous searches
+let searchHistory = JSON.parse(localStorage.getItem("searches"));
 
-        name.innerHTML = nameValue
-        temp.innerHTML = tempValue
-        discr.innerHTML = discrValue
-    })
+const callOpenWeather = (city) => {
+    // Creates URL for initial API call to retrieve latitude and longitude of requested city
+    const citySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=0656324568a33303c80afd015f0c27f8";
 
-
-
-    .catch(err => alert("Wrong city name!"))
-})
+    // Initial fetch to retrieve lat + lng
+    fetch(citySearch)
+    .then(function (response) {
+        // Handler if city is not found
+        if (!response.ok) {
+            currentForecastUl.innerHTML = "";
+            currentForecastH3.textContent = "Try again.";
+            const errorText = document.createElement("li");
+            errorText.textContent = "City not found.";
+            currentForecastUl.appendChild(errorText);
+            dailyForecastContainer.innerHTML = "";
+            fiveDay.classList.add("hidden");
+        } else {
+            // Converts API response into json object
+            response.json()
+        .then(function (data) {
+            // Pulls city name into variable for later
+            const cityName = data.name;
+        });
+    
+        searchForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+        });
